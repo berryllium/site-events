80dbde7e7d9983cb00b285ba2355c24696b79bd3
gorkun <dmitry.gorkun@oodji.com>
2024-03-03 18:33:58 +0100

    mvp

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.base')
 @section('content')
-    <div class="d-flex align-items-start">
+    <div class="row">
         @if($place)
             @if($place['image'])
-                <div class="image mw-50 me-5">
-                    <img src="{{ $place['image_src'] }}" alt="{{ $place['name'] }}">
+                <div class="col-6">
+                    <img src="{{ $place['image_src'] }}" alt="{{ $place['name'] }}" class="w-100">
                 </div>
             @endif
+        <div class="col-6 overflow-hidden">
             <table class="table table-striped">
                 <tr><th>Название:</th><td> {{ $place['name'] }} </td></tr>
                 <tr><th>Описание:</th><td> {{ $place['description'] }} </td></tr>
@@ -14,9 +15,8 @@
                 <tr><th>Описание:</th><td> {{ $place['description'] }} </td></tr>
                 <tr><th>Время работы:</th><td> {{ $place['working_hours'] }} </td></tr>
                 <tr><th>Дополнительная информация:</th><td> {!! $place['additional_info'] !!} </td></tr>
-                <tr><th>Ссылка для адреса:</th><td> {!! $place['address_link'] !!} </td></tr>
             </table>
-
+        </div>
 
         @else
             <div>Нет связи с сервером</div>
