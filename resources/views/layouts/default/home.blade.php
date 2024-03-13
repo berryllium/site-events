15df4f7d0ec4a5d4752a7cbe5f23ce19c8b9e63e
gorkun <dmitry.gorkun@oodji.com>
2024-03-13 17:24:53 +0100

    horizontal slider

--- resources/views/layouts/default/home.blade.php
+++ resources/views/layouts/default/home.blade.php
@@ -1,10 +1,18 @@
 @extends('base')
 @section('content')
+
     @if($horizontal_slider)
-    <div class="mb-5">
+    <div class="mb-5 d-none d-lg-block">
         <x-slider :files="$horizontal_slider"></x-slider>
     </div>
     @endif
+
+    @if($vertical_slider)
+        <div class="mb-5 d-block d-lg-none">
+            <x-slider :files="$vertical_slider"></x-slider>
+        </div>
+    @endif
+
     <div class="row">
         @foreach($posts as $data)
             <div class="col-12 col-md-6 col-lg-4 col-xl-3 mb-4">
