87d7da0ca5f9cca5ac7fbe92e4293f04838c4baa
gorkun <dmitry.gorkun@oodji.com>
2024-03-13 21:43:07 +0100

    а

--- resources/views/layouts/default/navbar.blade.php
+++ resources/views/layouts/default/navbar.blade.php
@@ -9,13 +9,13 @@
                 <a class="nav-link {{request()->segment(1) == '' ? 'active' : '' }}" href="{{ route('home') }}">Главная</a>
             </li>
             <li class="nav-item">
+                <a class="nav-link {{request()->segment(1) == 'about' ? 'active' : '' }}" href="{{ route('about') }}">О нас</a>
+            </li>
+            <li class="nav-item">
                 <a class="nav-link {{request()->segment(1) == 'news' ? 'active' : '' }}" href="{{ route('news') }}">Новости</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link {{request()->segment(1) == 'contacts' ? 'active' : '' }}" href="{{ route('contacts') }}">Контакты</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link {{request()->segment(1) == 'about' ? 'active' : '' }}" href="{{ route('about') }}">О нас</a>
             </li>
         </ul>
     </div>
