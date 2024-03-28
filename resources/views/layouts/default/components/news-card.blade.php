0a8303798d2811fa63622e87ba835d06f6c4d7de
gorkun <dmitry.gorkun@oodji.com>
2024-03-28 10:50:44 +0100

    f

--- resources/views/layouts/default/components/news-card.blade.php
+++ resources/views/layouts/default/components/news-card.blade.php
@@ -1,8 +1,8 @@
 <div class="card">
     <img src="{{ $img }}" class="card-img-top" alt="...">
     <div class="card-body">
-        <p><b>{{ $date }}</b></p>
         <p class="card-text">{{ $text }}</p>
+        <p><b>{{ \Illuminate\Support\Carbon::createFromTimeString($date)->format('d.m.Y H:i') }}</b></p>
         <a href="{{ route('detail', $id) }}" class="btn btn-primary">Подробнее</a>
     </div>
 </div>
