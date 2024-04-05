04330fbe790c6ea07d59232e4ab3e0e1fadec454
gorkun <dmitry.gorkun@oodji.com>
2024-04-05 22:19:48 +0200

    about

--- resources/views/layouts/svetofor/news.blade.php
+++ resources/views/layouts/svetofor/news.blade.php
@@ -1,5 +1,6 @@
 @extends('base')
 @section('content')
+    <div class="page-title mb-5"><span>Новости</span></div>
     <div class="row">
         @foreach($paginator as $data)
             <div class="col-12 col-md-6 col-lg-4 col-xl-3 mb-4">
