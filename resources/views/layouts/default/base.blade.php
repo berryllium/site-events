c9f05fbe9cdf5f34eb10aa3ace04858540dde138
gorkun <dmitry.gorkun@oodji.com>
2024-04-07 20:39:09 +0200

    seo

--- resources/views/layouts/default/base.blade.php
+++ resources/views/layouts/default/base.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @if($placeInfo['seo_description'])
+        <meta name="description" content="{{ $placeInfo['seo_description'] }}">
+    @endif
+    <title>{{ $placeInfo['seo_title'] ?? $placeInfo['name'] }}</title>
     <title>Default site</title>
     <link rel="icon" type="image/x-icon" href="{{ $placeInfo['logo_image_src'] ?? '' }}">
     @vite(['resources/css/app.scss', 'resources/views/layouts/'.config('app.template').'/css/style.scss', 'resources/js/app.js'])
