d13ecaea7014bfe1ea894b55975cf359eabfa5b8
gorkun <dmitry.gorkun@oodji.com>
2024-04-07 19:00:45 +0200

    f

--- resources/views/layouts/svetofor/home.blade.php
+++ resources/views/layouts/svetofor/home.blade.php
@@ -20,11 +20,7 @@
     </div>
 
     <div class="row">
-        @foreach($posts as $data)
-            <div class="col-12 col-md-6 col-lg-4 col-xl-3 mb-4">
-                <x-news-card :data="$data"></x-news-card>
-            </div>
-        @endforeach
+        <x-news-list :paginator="$posts"></x-news-list>
     </div>
 
     <div class="row">
