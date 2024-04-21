4bd5e0454c4ed07e0c3cda13721f20ee051c57e5
gorkun <dmitry.gorkun@oodji.com>
2024-04-21 17:49:10 +0200

    development center menu

--- resources/views/layouts/development_center/navbar.blade.php
+++ resources/views/layouts/development_center/navbar.blade.php
@@ -1,12 +1,11 @@
-<nav class="navbar navbar-expand-lg d-flex justify-content-between mx-5 mb-5">
+<nav class="navbar navbar-expand-lg d-flex mb-5">
     <div class="d-flex">
-        <a class="navbar-brand" href="{{ route('home') }}"><img width="64" src="{{ $placeInfo['logo_image_src'] }}" alt=""></a>
-        <div class="name text-uppercase ms-3">{{ $placeInfo['name'] }}</div>
+        <a class="navbar-brand" href="{{ route('home') }}"><img width="128" src="{{ $placeInfo['logo_image_src'] }}" alt=""></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
     </div>
-    <div class="collapse navbar-collapse  justify-content-end flex-grow-0 px-3" id="navbarNav">
+    <div class="collapse navbar-collapse  justify-content-end flex-grow-0" id="navbarNav">
         @include('nav')
     </div>
 </nav>
--- resources/views/layouts/development_center/top.blade.php
+++ resources/views/layouts/development_center/top.blade.php
@@ -1,17 +1,19 @@
 <div class="top-line justify-content-center d-flex">
-    <div class="contacts">
-        @if($placeInfo['phone'])
-            <a href="tel:{{ $placeInfo['phone'] }}" class="text-decoration-none me-3">
-                <i class="fas fa-phone me-2"></i> {{ $placeInfo['phone'] }}
-            </a>
-        @endif
-        @if($placeInfo['email'])
-            <a href="mailto:{{ $placeInfo['email'] }}" class="text-decoration-none me-3">
-                <i class="fas fa-envelope me-2"></i> {{ $placeInfo['email'] }}
-            </a>
-        @endif
-    </div>
+    <div class="wrap justify-content-center d-flex">
+        <div class="contacts">
+            @if($placeInfo['phone'])
+                <a href="tel:{{ $placeInfo['phone'] }}" class="text-decoration-none me-3">
+                    <i class="fas fa-phone me-2"></i> {{ $placeInfo['phone'] }}
+                </a>
+            @endif
+            @if($placeInfo['email'])
+                <a href="mailto:{{ $placeInfo['email'] }}" class="text-decoration-none me-3">
+                    <i class="fas fa-envelope me-2"></i> {{ $placeInfo['email'] }}
+                </a>
+            @endif
+        </div>
 
-    <div class="separator me-3">|</div>
-    <x-social-links :place="$placeInfo"></x-social-links>
+        <div class="separator me-3">|</div>
+        <x-social-links :place="$placeInfo"></x-social-links>
+    </div>
 </div>
